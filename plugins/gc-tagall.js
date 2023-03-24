dbd5055f421f9ab66c84a4b6a26c558615cd806b
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-03-24 00:40:47 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,17 +4,16 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*LA REVENTA DE ESTE BOT ESTA PROHIBIDA, PARA ADQUIRIRLO COMUNICARSE A :
-Wa.me/+56954390663* ${pesan}`
-let teks = `*RYU BOT LOS INVOCA BOTS*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let oi = `*@ventas.bots.gerixryu* ${pesan}`
+let teks = `*GeriPium los invoca plantitas🌱💖*\n\n ${oi}\n\n *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `🐼 @${mem.id.split('@')[0]}\n`}
-teks += `*└*@ffxryu.18\n\n*<3*`
+teks += `💤 @${mem.id.split('@')[0]}\n`}
+teks += `*@ventas.bots.gerixryu\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(tagall|invocar|todas|todos|todes)$/i
 handler.admin = true
 handler.group = true
 export default handler
