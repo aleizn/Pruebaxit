aeae8021b8fdd1b864f9a2bb53b14715d2a9f1ac
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2023-04-02 11:59:46 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,11 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*@ventas.bots.gerixryu* ${pesan}`
-let teks = `*GeriPium los invoca plantitas🌱💖*\n\n ${oi}\n\n *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let oi = `*@geripium* ${pesan}`
+let teks = `*𝙂𝙚𝙧𝙞𝙋𝙞𝙪𝙢 𝙩𝙚 𝙞𝙣𝙫𝙤𝙘𝙖 𝙛𝙖𝙣𝙩𝙖𝙨𝙢𝙖👻*\n\n ${oi}\n\n *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `💤 @${mem.id.split('@')[0]}\n`}
-teks += `*👑GeriPium*\n\n*<3*`
+teks += `💝🌸 @${mem.id.split('@')[0]}\n`}
+teks += `*🎀GeriPium*\n\n*<3*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
